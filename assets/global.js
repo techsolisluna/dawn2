1bdf7d6df876f9e5fc13b29a5bf04df3b5218605
Vinicius Maciel <vinissmaciel@gmail.com>
2023-06-29 17:07:33 -0300

    test newsletter email already subscribed

--- assets/global.js
+++ assets/global.js
@@ -1005,3 +1005,14 @@ class ProductRecommendations extends HTMLElement {
 }
 
 customElements.define('product-recommendations', ProductRecommendations);
+
+
+var live_url = window.location.href;
+var result = live_url.includes('form_type=customer');
+var input_val = document.querySelector('#NewsletterForm--footer').value.length;
+if(result && input_val != 0){
+    const add_ele = document.createElement("h3");
+    add_ele.innerText = "Esse e-mail já está inscrito!";
+    document.querySelector('.newsletter-form__field-wrapper').appendChild(add_ele);
+    add_ele.classList.add('newsletter_already_subscribe','newsletter-form__message','form__message');
+}
